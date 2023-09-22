4fc9079d50da3f29c127bc2dab2dbadfb05ee7db
shivasaicmi <shivasai.p@comakeit.com>
2023-09-22 12:25:56 +0530

    added sections to the landing page

--- credence-frontend/src/App.tsx
+++ credence-frontend/src/App.tsx
@@ -1,24 +1,10 @@
 import './App.css'
-import {useEffect} from "react";
-import { logger } from './helpers/loggers/logger';
-import {backend} from './lib/Network/HttpHelper';
-import { backendApiUrls } from './constants/backendApiEndpoints';
 
 function App() {
 
-  useEffect(()=>{
-      backend.get(backendApiUrls.data).then((response:unknown)=>{
-        logger.debug(response);
-      })
-      .catch((err)=>{
-        logger.debug(err)
-      })
-  })
-
   return (
-    <div>
-      <h1 className='bg-black' >hi</h1>
-    </div>
+    <>
+    </>
   )
 }
 
