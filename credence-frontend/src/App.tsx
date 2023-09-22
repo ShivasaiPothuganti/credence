7d3372ad362d755d36e0f09d28b4da8b37c640fe
shivasaicmi <shivasai.p@comakeit.com>
2023-09-22 12:33:37 +0530

    added basic router

--- credence-frontend/src/App.tsx
+++ credence-frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import './App.css'
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LandingPage from './pages/Landing/LandingPage';
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' Component={LandingPage} />
+      </Routes>
+    </Router>
   )
 }
 
