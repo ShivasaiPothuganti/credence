b81907ea6f76d960416515611f3882832b7d6ad2
ManikantaComakeit <manikanta.kaspa@comakeit.com>
2023-09-29 11:05:28 +0530

    modified styling

--- credence-frontend/src/pages/Landing/About/AboutSection.tsx
+++ credence-frontend/src/pages/Landing/About/AboutSection.tsx
@@ -29,7 +29,7 @@ function AboutSection() {
           </div>
           <div className="about_container_right relative h-screen w-[50%] flex justify-center items-center text-[white] bg-primaryBlack p-5 rounded-three">
           <div className="backgroundbox absolute h-16 w-16 rounded-3xl rotate-12 bg-primaryPurple top-[10%] right-[75%] "></div>
-          <div className="backgroundbox absolute h-[20rem] w-[20rem] bottom-[-5rem] right-[-5rem] rounded-[50%] bg-primaryPurple "></div>
+          <div className="backgroundbox absolute h-[12rem] w-[12rem] bottom-0 right-0 quarter-rounded-circle bg-primaryPurple "></div>
             <motion.div
               className="box"
               initial={{ opacity: 0, scale: 0.5 }}
